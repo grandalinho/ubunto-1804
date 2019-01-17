eefc84b7174687f9531dd2ec8074f26b8e281a2b
grandalinho <34448979+grandalinho@users.noreply.github.com>
2019-01-17 16:35:42 -0200

    Update atualizar.sh

--- atualizar.sh
+++ atualizar.sh
@@ -6,27 +6,37 @@ echo
 	sudo apt update
 echo
 echo Listas atualizadas
+sleep 5
+clear
 
 echo Atualizando os Software
 echo
 	sudo apt -y upgrade
 echo
 echo Software atualizados
+sleep 5
+clear
 
 echo Atualizando o Kernel
 echo
 	sudo apt -y dist-upgrade
 echo
 echo Kernel atualizado
+sleep 5
+clear
 
 echo Removendo Software
 echo
 	sudo apt -y autoremove
 echo
 echo Software removidos
+sleep 5
+clear
 
 echo Limpando o cache
 echo
 	sudo apt -y autoclean
 echo
-echo Cache limpo 
+echo Cache limpo
+sleep 5
+clear
